a2567d0922b2cad913b80d7a7bd3a1038d062121
lyp830414@163.com <lyp830414@163.com>
2019-02-08 00:20:07 -0800

    Add profile.4

---
+++ profile.4/Person.cc
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <iostream>
+#include <string>
+#include "Person.hpp"
+
+//Person::Person() const { => WRONG:  constructors may not be cv-qualified
+	                     // DO NOT ALLOW THE CONSTRUCT FUNC USE 'const' in the construct func
+Person::Person() : const_val1("aa"), const_val2("cc"), name(""), address("") {//const value only allowed declare at place here 
+	cout<<"HERE 1"<<endl;
+	this->name = "";
+	this->address = "";
+
+	cout<<"+++  INIT: pubname: "<<pubname<<", const_val1: "<<const_val1<<", const_val2: "<<const_val2<<endl;
+}
+
+Person::Person(string name, string address) : const_val1("aa"), const_val2("cc") { //const value only allowed declare at place here 
+	cout<<"HERE 2"<<endl;
+	this->name = name;
+	this->address = address;
+	
+	cout<<"!!!! INIT: const_val1: "<<const_val1<<", const_val2: "<<const_val2<<endl;
+}
+
+//Person abc() {return new Person};
+
+
+int main(int argc, char *argv[]) {
+	
+	Person abc1{}; //OK, it will use Person()
+	Person abc2("11", "22");//OK
+	Person2 abc3{}; //OK, it will use default constructure
+	Person4 abc4{}; //OK. it will use default constructure-> Person4()=default;	
+
+	Person abc(); // WRONG, this will not call any constructure, because it is not a class-object, it is a FUNCTION DECLARE, So if you use abc.** will compile wrong!!!
+	
+
+	//cout<<person2.name<<endl; -->WRONG
+	//cout<<person4.name<<endl; -->WRONG
+	return 0;
+}
---
+++ profile.4/Person.hpp
@@ -0,0 +1,47 @@
+using namespace std;
+
+class Person {
+public:
+	//Person() const;---> error: constructors may not be cv-qualified
+	Person();
+	Person(string name, string address);// const_val1("const_val1");
+	~Person(){}
+	
+	string pubname="33";
+private:
+	
+	string name = "init_name";
+	string address = "init_address";
+	
+	const string const_val1 = "init_const_val1";
+	const string const_val2 = "init_const_val2";	
+};
+
+
+class Person2 {
+public:
+	string name;
+private:
+
+	string test;
+};
+
+class Person3 {
+public:
+	Person3(string name){cout<<"HERE person3!"<<endl;}
+	string value="newvalue";
+	void hello(){cout<<"Hello!"<<endl;}
+private:
+
+	string test;
+};
+
+class Person4 {
+
+public:
+	Person4()= default; //SAME AS THE DEFAULT CONSTRUCTURE
+	
+	string name;//="init_test_p4";
+private:
+	string mytest;
+};
