92eca83223a2ce9ec3b7dd92e6c3dde433e3092b
lyp830414 <lyp830414@163.com>
2019-03-18 16:30:39 +0800

    update for member-pointer of func calling

--- profile.11/test11.hpp
+++ profile.11/test11.hpp
@@ -22,7 +22,7 @@ class A {
 		A()=default;
 		virtual void test(){}
 		int pub_abc = 22;
-		int pub_func(void){return 1;}
+		int pub_func(void){cout<<"a pub func"<<endl;return 1;}
 	private:
 		int pri_abc = 0;
 		int pri_func(void){return 1;}
@@ -40,8 +40,17 @@ class B {
 
 		auto move_ptr(unique_ptr<A> new_ptra)->A* {pa = std::move(new_ptra); return pa.get();}
 		
+		using A_PRIFUNC = decltype(&A::pub_func);
+
 		int A::*pa_abc = &A::pub_abc;
 		int B::*pb_ccc = &B::prib_ccc;
+
+		A_PRIFUNC pa_func1 = &A::pub_func;
+		//wrong
+		//int A::(*pa_func11)(void) = &A::pub_func;
+		
+		//ok
+		int (A::*pa_func11)(void) = &A::pub_func;
 		
 		auto test_ptr() ->void {
 			cout<<"\n\n=========>=================>"<<endl;
@@ -54,6 +63,18 @@ class B {
 			cout<<"<=========<=================<\n\n";
 			//wrong. do not support this usage.
 			//cout<<get_ptr()->&pa_abc<<endl;
+
+			(get_ptr()->*pa_func1)();
+			(get_ptr()->*pa_func11)();
+			
+			cout<<"this->*pb_ccc"<<endl;
+			cout<<this->*pb_ccc<<endl;
+			this->*pb_ccc = 20;
+			cout<<this->*pb_ccc<<endl;
+			cout<<this->prib_ccc<<endl;
+			this->prib_ccc= 23;
+			cout<<this->prib_ccc<<endl;
+			cout<<this->*pb_ccc<<endl;
 		}
 	
 	private:
